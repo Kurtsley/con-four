84041363f9c44c681ccb3eb7671127d59931fede
Kurtsley <kurtsley@gmail.com>
2024-09-06 00:23:26 -0500

    Reduce board size

--- main.py
+++ main.py
@@ -2,27 +2,28 @@
 
 import pyxel
 
+SCREEN_HEIGHT = 180
+SCREEN_WIDTH = 180
+
 PADDING_Y = 40
 
-BOARD_WIDTH = 160
-BOARD_HEIGHT = 152
+TOKEN_SIZE = 16
+TOKEN_GRAVITY = 4
+
+BOARD_WIDTH = 124
+BOARD_HEIGHT = 112
 BOARD_OFFSET = 6
-BOARD_BOTTOM = 172
-BOARD_RIGHT_EDGE = 168
-BOARD_LEFT_EDGE = 36
+BOARD_BOTTOM = BOARD_HEIGHT + PADDING_Y - TOKEN_SIZE
+BOARD_RIGHT_EDGE = 138
+BOARD_LEFT_EDGE = 28
 
 TABLE_HEIGHT = BOARD_HEIGHT + PADDING_Y
 
-TOKEN_WIDTH = 16
-TOKEN_HEIGHT = 16
-TOKEN_GRAVITY = 4
-TOKEN_OFFSET_X = TOKEN_WIDTH + BOARD_OFFSET
-
-INITIAL_SPAWN_X = 102
+INITIAL_SPAWN_X = 82
 INITIAL_SPAWN_Y = 18
 
 PLAYER_COLOR_1 = 8
-PLAYER_COLOR_2 = 6
+PLAYER_COLOR_2 = 10
 
 
 class Board:
@@ -38,30 +39,15 @@ class Board:
 
     def drawBackground(self):
         pyxel.text(BOARD_OFFSET, BOARD_OFFSET, "Connect 4", 7)
-        pyxel.rect(self.padding_x, PADDING_Y, BOARD_WIDTH, BOARD_HEIGHT, 10)
+        pyxel.rect(self.padding_x, PADDING_Y, BOARD_WIDTH, BOARD_HEIGHT, 5)
         pyxel.rect(0, TABLE_HEIGHT, pyxel.width, pyxel.height, 9)
 
     def drawGrid(self):
         for row in range(6):
             for col in range(7):
-                self.slot_x = self.padding_x + BOARD_OFFSET + col * TOKEN_OFFSET_X
-                self.slot_y = PADDING_Y + BOARD_OFFSET + row * (TOKEN_HEIGHT + BOARD_OFFSET)
-                self.slot_id = (self.slot_x, self.slot_y)
-                pyxel.rect(self.slot_x, self.slot_y, TOKEN_WIDTH, TOKEN_HEIGHT, 0)
-                self.slots.append(self.slot_id)
-
-    def createTopTokenSlots(self):
-        for col in range(7):
-            self.slot_x = self.padding_x + BOARD_OFFSET + col * TOKEN_OFFSET_X
-            self.slot_y = (PADDING_Y - TOKEN_HEIGHT) - BOARD_OFFSET
-            self.topSlot_id = (self.slot_x, self.slot_y)
-            self.topSlots.append(self.topSlot_id)
-
-    def updateSlots(self, tokens):
-        for slot in self.slots:
-            for token in tokens:
-                if token.x == slot[0] and token.y == slot[1] and token.landed:
-                    self.takenSlots.append(slot)
+                self.slot_x = self.padding_x + BOARD_OFFSET + col * TOKEN_SIZE
+                self.slot_y = PADDING_Y + row * TOKEN_SIZE
+                pyxel.rect(self.slot_x, self.slot_y, TOKEN_SIZE, TOKEN_SIZE, 0)
 
 
 class Token:    
@@ -83,7 +69,7 @@ class Token:
             self.y += TOKEN_GRAVITY
 
             if self.isLanded():
-                self.y = BOARD_BOTTOM - TOKEN_HEIGHT
+                self.y = BOARD_BOTTOM - TOKEN_SIZE
                 self.dropped = False
                 self.landed = True
 
@@ -92,25 +78,25 @@ class Token:
             if self.x == slot[0] and self.y == slot[1]:
                 return True
 
-        if self.y >= BOARD_BOTTOM - TOKEN_HEIGHT:
+        if self.y >= BOARD_BOTTOM - TOKEN_SIZE:
             return True
 
     def updateMovement(self):
-        if pyxel.btnp(pyxel.KEY_RIGHT) and self.x <= BOARD_RIGHT_EDGE - TOKEN_WIDTH and not self.dropped:
-            self.x += TOKEN_OFFSET_X
-        elif pyxel.btnp(pyxel.KEY_LEFT) and self.x >= BOARD_LEFT_EDGE + TOKEN_WIDTH and not self.dropped:
-            self.x -= TOKEN_OFFSET_X
+        if pyxel.btnp(pyxel.KEY_RIGHT) and self.x <= BOARD_RIGHT_EDGE - TOKEN_SIZE and not self.dropped:
+            self.x += TOKEN_SIZE
+        elif pyxel.btnp(pyxel.KEY_LEFT) and self.x >= BOARD_LEFT_EDGE + TOKEN_SIZE and not self.dropped:
+            self.x -= TOKEN_SIZE
         elif pyxel.btnp(pyxel.KEY_SPACE):
             self.dropped = True
 
 
     def draw(self):
-        pyxel.rect(self.x, self.y, TOKEN_WIDTH, TOKEN_HEIGHT, self.player_color)
+        pyxel.rect(self.x, self.y, TOKEN_SIZE, TOKEN_SIZE, self.player_color)
 
 
 class App:
     def __init__(self):
-        pyxel.init(220, 220, title="Connect 4")
+        pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Connect 4")
 
         self.tokens = []
         self.landedTokens = []
@@ -133,8 +119,6 @@ class App:
             self.spawnToken(self.board)
         for token in self.tokens:
             token.update()
-        self.board.updateSlots(self.tokens)
-        print(len(self.board.takenSlots))
 
 
 
