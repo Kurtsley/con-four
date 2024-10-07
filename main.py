a39a7df223b075d6f5ba300a3145a15eee64fdab
Kurtsley <kurtsley@gmail.com>
2024-10-06 23:22:06 -0500

    Fix victory condition

--- main.py
+++ main.py
@@ -48,49 +48,61 @@ class Board:
                 col_slots.append(self.slot_id)
             self.slots.append(col_slots)
 
-    def checkGrid(self,tokens):
+    def checkGrid(self, tokens):
         for token in tokens:
             if token.landed and token not in self.landedTokens:
                 self.landedTokens.append(token)
 
-    def checkVictory(self):
+    def checkVictory(self, tokens):
         for row in range(len(self.slots)):
             for col in range(len(self.slots[row])):
                 slot_x = self.slots[row][col][0]
                 slot_y = self.slots[row][col][1]
                 slot_color = pyxel.pget(slot_x + BOARD_OFFSET, slot_y)
-                
+
                 if slot_color == 0:
                     continue
 
                 if col <= len(self.slots[row]) - 4:
-                    if all(pyxel.pget(self.slots[row][col + i][0], self.slots[row][col + i][1]) \
+                    if all(pyxel.pget(self.slots[row][col + i][0], self.slots[row][col + i][1])
                            == slot_color for i in range(4)):
-                        return True
+                        for token in tokens:
+                            if token.x == slot_x and token.y == slot_y and token.landed:
+                                return True
+                                break
 
                 if row <= len(self.slots) - 4:
-                    if all(pyxel.pget(self.slots[row + i][col][0], self.slots[row + i][col][1]) \
+                    if all(pyxel.pget(self.slots[row + i][col][0], self.slots[row + i][col][1])
                            == slot_color for i in range(4)):
-                        return True
+                        for token in tokens:
+                            if token.x == slot_x and token.y == slot_y and token.landed:
+                                return True
+                                break
 
                 if row <= len(self.slots) - 4 and col <= len(self.slots[row]) - 4:
-                    if all(pyxel.pget(self.slots[row + i][col + i][0], self.slots[row + i][col + i][1]) \
+                    if all(pyxel.pget(self.slots[row + i][col + i][0], self.slots[row + i][col + i][1])
                            == slot_color for i in range(4)):
-                        return True
+                        for token in tokens:
+                            if token.x == slot_x and token.y == slot_y and token.landed:
+                                return True
+                                break
 
                 if row <= len(self.slots) - 4 and col >= 3:
-                    if all(pyxel.pget(self.slots[row + i][col - i][0], self.slots[row + i][col - i][1]) \
+                    if all(pyxel.pget(self.slots[row + i][col - i][0], self.slots[row + i][col - i][1])
                            == slot_color for i in range(4)):
-                        return True
-                    
+                        for token in tokens:
+                            if token.x == slot_x and token.y == slot_y and token.landed:
+                                return True
+                                break
+
         return False
 
     def hasSpawnToken(self, tokens):
         for token in tokens:
             if token.freshSpawn:
                 return True
-            
-        return False        
+
+        return False
 
     def drawBackground(self):
         pyxel.text(BOARD_OFFSET, BOARD_OFFSET, "Connect 4", 7)
@@ -105,7 +117,7 @@ class Board:
                 pyxel.rect(slot_x, slot_y, TOKEN_SIZE, TOKEN_SIZE, 0)
 
 
-class Token:    
+class Token:
     def __init__(self, x, y, count):
         self.x = x
         self.y = y
@@ -125,10 +137,10 @@ class Token:
             self.fallingToken = self
             self.y += TOKEN_GRAVITY
             self.checkCollisions(tokens)
-    
+
     def hasLanded(self):
         return self.y >= BOARD_BOTTOM - TOKEN_SIZE
-    
+
     def canDrop(self, board):
         topSlots = []
         for col in range(7):
@@ -138,10 +150,10 @@ class Token:
         for token in board.landedTokens:
             tokenTuple = (token.x, token.y)
             if tokenTuple in topSlots and self.x == token.x:
-                    return False
-            
+                return False
+
         return True
-    
+
     def checkCollisions(self, tokens):
         if self.hasLanded():
             self.y = BOARD_BOTTOM - TOKEN_SIZE
@@ -150,11 +162,11 @@ class Token:
             self.fallingToken = None
 
         for other_token in tokens:
-            if (self != other_token and 
-                self.dropped and not 
-                other_token.dropped and not 
-                other_token.freshSpawn
-            ):
+            if (self != other_token and
+                    self.dropped and not
+                    other_token.dropped and not
+                    other_token.freshSpawn
+                ):
                 if self.y + TOKEN_SIZE >= other_token.y and self.x == other_token.x:
                     self.y = other_token.y - TOKEN_SIZE
                     self.dropped = False
@@ -163,14 +175,14 @@ class Token:
 
     def updateMovement(self, board):
         canDrop = self.canDrop(board)
-        if (pyxel.btnp(pyxel.KEY_RIGHT) and 
-            self.x <= BOARD_RIGHT_EDGE - TOKEN_SIZE and not 
-            self.dropped
-        ):
+        if (pyxel.btnp(pyxel.KEY_RIGHT) and
+                self.x <= BOARD_RIGHT_EDGE - TOKEN_SIZE and not
+                self.dropped
+            ):
             self.x += TOKEN_SIZE
 
-        elif (pyxel.btnp(pyxel.KEY_LEFT) and 
-              self.x >= BOARD_LEFT_EDGE + TOKEN_SIZE and not 
+        elif (pyxel.btnp(pyxel.KEY_LEFT) and
+              self.x >= BOARD_LEFT_EDGE + TOKEN_SIZE and not
               self.dropped):
             self.x -= TOKEN_SIZE
 
@@ -195,7 +207,7 @@ class App:
         self.board.createGrid()
 
         self.spawnToken()
-        
+
         pyxel.run(self.update, self.draw)
 
     def spawnToken(self):
@@ -215,11 +227,11 @@ class App:
             for token in self.tokens:
                 token.update(self.tokens, self.board)
                 if not token.fallingToken and not self.board.hasSpawnToken(self.tokens) \
-                    and not token.landed:
+                        and not token.landed:
                     self.spawnToken()
                     break
             self.board.checkGrid(self.tokens)
-            if self.board.checkVictory():
+            if self.board.checkVictory(self.tokens):
                 self.game_over = True
         else:
             if pyxel.btnp(pyxel.KEY_SPACE):
@@ -228,8 +240,25 @@ class App:
     def draw(self):
         pyxel.cls(0)
         if self.game_over:
-            pyxel.text(pyxel.width // 2, BOARD_OFFSET, "Press SPACE to reset", pyxel.COLOR_RED)
-            
+            for token in self.tokens:
+                if token.freshSpawn:
+                    color = token.player_color
+                    player1_victory = "Player 1 Wins!"
+                    player2_victory = "Player 2 Wins!"
+
+                    # The color is reversed becuase I am checking for
+                    # the color of the spawn token, which is the opposite
+                    # of the winning color...
+                    if color == PLAYER_COLOR_1:
+                        pyxel.text(pyxel.width // 2 - (len(player2_victory) * 2), BOARD_OFFSET,
+                                   player2_victory, PLAYER_COLOR_2)
+
+                    else:
+                        pyxel.text(pyxel.width // 2 - (len(player1_victory) * 2), BOARD_OFFSET,
+                                   player1_victory, PLAYER_COLOR_1)
+
+            pyxel.text(pyxel.width // 2 + 20, BOARD_OFFSET + 15,
+                       "SPACE to reset", pyxel.COLOR_LIME)
 
         self.board.drawBackground()
         self.board.drawGrid()
